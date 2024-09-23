c6e1d77395f806dc0c399cd1b09e17d033ca1cd0
oxscar93 <oscar_master93@hotmail.com>
2024-09-22 23:06:35 -0300

    refactors

---
+++ app/handlers/screenshot.js
@@ -0,0 +1,58 @@
+const { ipcMain, dialog } = require('electron');
+const puppeteer = require('puppeteer');
+const validUrl = require('../utils/utils')
+
+const setupScreenshotHandler = () => {
+    ipcMain.on('take-screenshot', async (event, formData) => {
+        if (!validUrl(formData.url, ['http', 'https'])){
+            event.reply('error', 'Invalid url entered.');
+            return;
+        }
+
+        try {
+            const browser = await puppeteer.launch({headless: false});
+            const [page] = await browser.pages();
+
+            await page.goto(formData.url);
+
+            const fileFormat = formData.fileFormat;
+            const filePath = await getFilePathFromDialog(fileFormat);
+        
+            if (!filePath){
+                event.reply('output', 'Canceled');
+                await browser.close();
+                return;
+            }
+       
+            await saveScreenShot(page, filePath, fileFormat);
+            await browser.close();
+
+            event.reply('output', 'Screenshot saved successfully!');         
+        } catch (error) {
+            console.error(error);
+            event.reply('error', 'Error occurred: ' + error.message);
+        }
+    });
+
+    const getFilePathFromDialog = async (fileFormat) => {
+        const { filePath } = await dialog.showSaveDialog({
+            title: 'Save Screenshot file',
+            filters: [
+                { name: 'Images', extensions: [fileFormat] },
+            ],
+        });
+
+        return filePath;
+    }
+    
+    const saveScreenShot = async (page, filePath, fileFormat) =>{
+        if (fileFormat == 'pdf'){
+            await page.pdf({ path: filePath, format: 'A4' });
+        }
+        else{
+            await page.screenshot({ path: filePath }); 
+        }
+    }
+}
+
+module.exports = setupScreenshotHandler;
---
+++ app/main.js
@@ -0,0 +1,37 @@
+
+const { app, BrowserWindow} = require('electron');
+const path = require('path');
+const setupScreenshotHandler = require('./handlers/screenshot');
+
+function setHandlers(){
+    setupScreenshotHandler();
+}
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true,
+        },
+    });
+
+    mainWindow.loadFile('app/index.html');
+}
+
+setHandlers();
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
